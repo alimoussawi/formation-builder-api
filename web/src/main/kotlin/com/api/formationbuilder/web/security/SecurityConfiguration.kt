0cc2d1c96423430e3fa05f741b9ad6ccfbe773db
alimoussawi <ali>
2023-04-15 17:22:32 +0200

    configure oauth2-resource-server

---
+++ web/src/main/kotlin/com/api/formationbuilder/web/security/SecurityConfiguration.kt
@@ -0,0 +1,30 @@
+package com.api.formationbuilder.web.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+open class SecurityConfiguration {
+    @Bean
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { auth ->
+                auth
+                    .anyRequest().authenticated()
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .oauth2ResourceServer().jwt()
+                    .and()
+                    .and()
+                    .cors()
+                    .and()
+                    .csrf().disable()
+            }
+        return http.build()
+    }
+}
